b6c8a7942da756de5a27eb514713a7d1583a6a57
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-07-10 00:11:35 -0400

    updated index.js

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,35 @@
-import {useSession, getProviders, getSession} from 'next-auth/react';
+import { useSession, getProviders, getSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import Sidebar from '@/components/Sidebar';
 import Center from '@/components/Center';
 
 export default function Home() {
-    return (
-      <div className='bg-black h-screen overflow-hidden'>
-        <main className='flex'>
-            <Sidebar />
-            <Center />
-        </main>
-      </div>
-    );
+  const [session, loading] = useSession(); // Removed ": session" from the declaration
+
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && !session) {
+      router.push('/login'); // Redirect to "/login" if no session
+    }
+  }, [loading, session, router]);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
-export async function getServerSideProps(context){
+  return (
+    <div className='bg-black h-screen overflow-hidden'>
+      <main className='flex'>
+        <Sidebar />
+        <Center />
+      </main>
+    </div>
+  );
+}
+
+export async function getServerSideProps(context) {
   const providers = await getProviders();
   const session = await getSession(context);
 

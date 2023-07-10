136efd8b3c5ff8e0b393a597be8e8fcec4b7f113
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-07-10 00:17:17 -0400

    updated index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3071ad404b46a941"; }
+/******/ 		__webpack_require__.h = function() { return "c3d1729c25c241d9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,15 @@ import Sidebar from '@/components/Sidebar';
 import Center from '@/components/Center';
 
 export default function Home() {
-  const [session, loading] = useSession(); // Removed ": session" from the declaration
+  const session = useSession(); // Removed ": session" from the declaration
 
   const router = useRouter();
 
   useEffect(() => {
-    if (!loading && !session) {
+    if (!session) {
       router.push('/login'); // Redirect to "/login" if no session
     }
-  }, [loading, session, router]);
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
+  }, [session, router]);
 
   return (
     <div className='bg-black h-screen overflow-hidden'>

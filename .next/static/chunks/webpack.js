75d622b9ce1e3fec782754c65b81683d11045e42
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-07-10 21:04:14 -0400

    updated index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3d1729c25c241d9"; }
+/******/ 		__webpack_require__.h = function() { return "79167eef9e2066ad"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

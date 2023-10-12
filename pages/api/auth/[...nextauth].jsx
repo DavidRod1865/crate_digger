729b4aa758f09582cd78b4cbdb60817be374988c
David Rodriguez <93050365+DavidRod1865@users.noreply.github.com>
2023-10-11 22:03:10 -0400

    Updated NextAuth

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -33,6 +33,7 @@ export default NextAuth({
         clientId: process.env.NEXT_PUBLIC_CLIENT_ID,
         clientSecret: process.env.NEXT_PUBLIC_CLIENT_SECRET,
         authorizationURL: LOGIN_URL,
+        callbackURL: "https://crate-digger-mu.vercel.app/api/auth/callback/spotify",
       }),
     ],
     secret: process.env.NEXT_PUBLIC_JWT_SECRET,
